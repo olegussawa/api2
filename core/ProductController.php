504509a2eff89558a043cd3080b653144f1e6eb9
oleg <beavercreek@mail.ru>
2024-09-28 03:43:48 +0700

    добавил методы update и delete

--- core/ProductController.php
+++ core/ProductController.php
@@ -2,119 +2,141 @@
 
 
 
-class ProductController{
-
-public function __construct(private productGateway $gateway)
+class ProductController
 {
-    
-}
 
-
-    public function processrequest($method,$id=null):void
+    public function __construct(private productGateway $gateway)
     {
-        if($id){
-$this->ProcessResouceRequest($method,$id);
-        }else {
+    }
+
+
+    public function processrequest($method, $id = null): void
+    {
+        if ($id) {
+            $this->ProcessResouceRequest($method, $id);
+        } else {
             $this->ProcessCollectionRequest($method);
         }
     }
 
 
 
-private function ProcessResouceRequest($method,$id){
+    private function ProcessResouceRequest($method, $id)
+    {
 
-$product=$this->gateway->getone($id);
-//проверим если нет продукта с этим номером выходим из функции с 404
-if(!$product){
-    http_response_code(404);
-    echo json_encode(['message'=>'product not found']);
-    return;
+        $product = $this->gateway->getone($id); //massive
+        //проверим если нет продукта с этим номером выходим из функции с 404
+        if (!$product) {
+            http_response_code(404);
+            echo json_encode(['message' => 'product not found']);
+            return;
+        }
+
+        switch ($method) {
+            case "GET":
+                echo json_encode($product);
+                break;
+
+
+            case "PUT":
+                $newdata = json_decode(file_get_contents("php://input")); //object
+
+                //var_dump($newdata);
+                //если массив с ошибками не пустой то выводим джсон с ошибками и завершаем код
+                $err = $this->validation_data($newdata, false);
+                if (!empty($err)) {
+                    echo json_encode(['error' => $err]);
+                    break;
+                }
+
+                $rows = $this->gateway->update($newdata, $product, $id);
+
+
+
+                echo json_encode([
+                    'message' => "product $id updated",
+                    'rows' => $rows
+                ]);
+
+                break;
+            case "DELETE":
+                $rows = $this->gateway->delete($id);
+                echo json_encode([
+                    'message' => "product $id deleted",
+                    'rows' => $rows
+                ]);
+
+                break;
+            default:
+                http_response_code(405);
+                header("Allow: GET,PUT,DELETE");
+        }
+    }
+
+
+
+    private function ProcessCollectionRequest($method)
+    {
+        switch ($method) {
+            case "GET":
+
+                print_r(json_encode($this->gateway->getall()));
+                break;
+
+            case "POST":
+                $data = json_decode(file_get_contents("php://input"));
+
+
+
+
+                //если массив с ошибками не пустой то выводим джсон с ошибками и завершаем код
+                $err = $this->validation_data($data);
+                if (!empty($err)) {
+                    echo json_encode(['error' => $err]);
+                    break;
+                }
+
+
+
+                $id = $this->gateway->create($data);
+
+                //код создания записи
+                http_response_code(201);
+
+                echo json_encode([
+                    'message' => 'post created',
+                    'id' => $id
+                ]);
+
+                break;
+            default:
+                http_response_code(405);
+                header("Allow: GET,POST");
+        }
+    }
+
+
+
+
+
+
+
+
+    private function validation_data($data, $is_new = true)
+    {
+
+        $errors = [];
+        if ($is_new && empty($data->name)) {
+            $errors[] = 'The name is required';
+        }
+        if ($is_new && empty($data->price)) {
+            $errors[] = 'The price is required';
+        }
+
+        if ($is_new && empty($data->number)) {
+            $errors[] = 'The number is required';
+        }
+
+        return $errors;
+    }
 }
-
-switch($method){
-    case "GET":
-        echo json_encode($product);
-break;
-
-
-}
-}
-
-
-
-private function ProcessCollectionRequest($method){
-switch($method){
-    case "GET":
-        
-print_r(json_encode($this->gateway->getall())) ;
-break;
-
-case "POST":
-    $data =json_decode(file_get_contents("php://input"));
-
-
-
-
-//если массив с ошибками не пустой то выводим джсон с ошибками и завершаем код
-$err=$this->validation_data($data);
-if(!empty($err)){
-    echo json_encode(['error'=>$err]);
-    break;
-}
-
-
-
-    $id=$this->gateway->create($data);
-
-   //код создания записи
-   http_response_code(201);
-
-   echo json_encode([
-    'message'=>'post created',
-    'id'=>$id
-   ]);
-   
-   break;
-   default:http_response_code(405);
-   header("Allow: GET,POST");
-}
-
-
-}
-   
-
-
-
-
-
-
-
-private function validation_data($data){
-
-$errors=[];
-if(empty($data->name)){
-    $errors[]='The name is required';
-}
-if(empty($data->price)){
-    $errors[]='The price is required';
-}
-
-if(empty($data->number)){
-    $errors[]='The number is required';
-}
-
-return $errors;
-}
-
-
-
-
-
-
-
-
-}
-
-
-
-
--- core/ProductGateway.php
+++ core/ProductGateway.php
@@ -3,64 +3,80 @@
 
 
 
-class ProductGateway{
-
-private PDO $conn;
-
-public function __construct(Database $database) {
-    $this->conn = $database->getconnection();
-}
-
-public function getall(){
-
-    $query="SELECT * FROM продукты";
-
-    $sth = $this->conn->prepare($query);
-    $sth->execute();
-    
-$arr = $sth->fetchAll(PDO::FETCH_ASSOC);
-$art['data']=$arr;
-return $art;
-}
-
-public function create($data)
+class ProductGateway
 {
 
+    private PDO $conn;
 
-
-    $sth = $this->conn->prepare("INSERT INTO `продукты` SET `name` = :name, `price` = :price, `number`= :number");
-    //clean data
-   
-    // $data->name=htmlspecialchars(strip_tags($data->name));
-    // $data->price=htmlspecialchars(strip_tags($data->price));
-    // $data->number=htmlspecialchars(strip_tags($data->number));
-
-    if($sth->execute(['name'=>$data->name, 'price'=>$data->price,'number'=>$data->number])){
-
-        return $this->conn->lastInsertId();
-        
+    public function __construct(Database $database)
+    {
+        $this->conn = $database->getconnection();
     }
-    
-return false;
+
+    public function getall()
+    {
+
+        $query = "SELECT * FROM продукты";
+
+        $sth = $this->conn->prepare($query);
+        $sth->execute();
+
+        $arr = $sth->fetchAll(PDO::FETCH_ASSOC);
+        $art['data'] = $arr;
+        return $art;
+    }
+
+    public function create($data)
+    {
+
+
+
+        $sth = $this->conn->prepare("INSERT INTO `продукты` SET `name` = :name, `price` = :price, `number`= :number");
+        //clean data
+
+        // $data->name=htmlspecialchars(strip_tags($data->name));
+        // $data->price=htmlspecialchars(strip_tags($data->price));
+        // $data->number=htmlspecialchars(strip_tags($data->number));
+
+        if ($sth->execute(['name' => $data->name, 'price' => $data->price, 'number' => $data->number])) {
+
+            return $this->conn->lastInsertId();
+        }
+
+        return false;
+    }
+
+    public function getone($id): array | false
+    {
+        $sth = $this->conn->prepare("SELECT * FROM `продукты` WHERE `id` = :id");
+        $sth->execute(['id' => $id]);
+        //$sth->execute(['id'=>3]);
+        $arr = $sth->fetch(PDO::FETCH_ASSOC);
+        return $arr;
+    }
+
+
+    public function update($new, $product, $id)
+    {
+        $sth = $this->conn->prepare("UPDATE `продукты` SET `name` = :n, `price` = :p,`number`= :num WHERE `id` = :id");
+        $sth->execute([
+            'n' => $new->name ?? $product['name'], 'p' => $new->price ?? $product['price'], 'num' => $new->number ?? $product['number'],
+            'id' => $id
+        ]);
+
+        return $sth->rowCount();
+    }
+
+
+    public function delete($id)
+    {
+        $sth =  $this->conn->prepare("DELETE FROM `продукты` WHERE `id` = :id");
+
+
+
+        $sth->execute(['id' => $id]);
+
+
+        return $sth->rowCount();
+    }
 }
-
-public function getone($id): array | false
-{
-    $sth = $this->conn->prepare("SELECT * FROM `продукты` WHERE `id` = :id");
-    $sth->execute(['id'=>$id]);
-    //$sth->execute(['id'=>3]);
-    $arr = $sth->fetch(PDO::FETCH_ASSOC);
-    return $arr;
-}
-
-
-}
-
-
-
-
-
-
-
-
-?>
